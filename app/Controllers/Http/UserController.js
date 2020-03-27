97b2548404985d69d698674db99bf82346cbd7a0
Bruno Jutkoski <bjutkoski@MacBook-Pro-de-Bruno.local>
2020-03-27 02:03:25 -0300

    file upload

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,10 +3,28 @@
 const User = use('App/Models/User')
 
 class UserController {
+  async index ({ request }) {
+    const users = await User
+      .query()
+      .with('file')
+      .orderBy('birthday')
+      .fetch()
+    return users
+  }
+
   async store ({ request }) {
-    const data = request.only(['username', 'email', 'password', 'admin'])
+    const data = request.only([
+      'username',
+      'email',
+      'password',
+      'description',
+      'birthday',
+      'file_id',
+      'admin'
+    ])
 
     const user = await User.create(data)
+    await user.load('file')
 
     return user
   }
--- start/routes.js
+++ start/routes.js
@@ -1,6 +1,13 @@
-'use strict';
+'use strict'
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+Route.get('files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+  Route.get('users', 'UserController.index')
+}).middleware(['auth'])
